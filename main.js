f29fb0a54108436531925364b203d24cbb8fd014
hesshess <64644262+hesshess@users.noreply.github.com>
2020-08-29 22:31:06 +0100

    JS_stop

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ const gameBtn = document.querySelector('.game__button')
 const gameTimer = document.querySelector('.game__timer')
 const gameScore = document.querySelector('.game__score')
 
+const popUp = document.querySelector('.pop-up')
+const popUpMessage = document.querySelector('.pop-up__message')
+const popUpRefresh = document.querySelector('.pop-up__refresh ')
+
 let started = false
 let score = 0
 let timer = undefined
@@ -30,12 +34,20 @@ function startGame () {
   startGameTimer()
 }
 
-function stopGame () {}
+function stopGame () {
+  stopGameTimer()
+  hideGameBtn()
+  showPopUpWithText('REPLAY❓')
+}
 
 function showStopBtn () {
   const icon = gameBtn.querySelector('.fa-play')
   icon.classList.add('fa-stop')
   icon.classList.remove('fa-play')
+}
+
+function hideGameBtn () {
+  gameBtn.style.visibility = 'hidden'
 }
 
 function showTimerAndScore () {
@@ -55,11 +67,21 @@ function startGameTimer () {
   }, 1000)
 }
 
+function stopGameTimer () {
+  clearInterval(timer)
+}
+
 function updateTimerText (time) {
   const minutes = Math.floor(time / 60)
   const seconds = time % 60
   gameTimer.innerText = `${minutes}:${seconds}`
 }
+
+function showPopUpWithText (text) {
+  popUpMessage.innerText = text
+  popUp.classList.remove('pop-up--hide')
+}
+
 function initGame () {
   field.innerHTML = ''
   gameScore.innerText = CARROT_COUNT
