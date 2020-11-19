cb875efc2fcd9489ed106c600aa0e2fe66f2cb07
hesshess <64644262+hesshess@users.noreply.github.com>
2020-11-19 17:15:06 +0000

    Javascript Refactoring

---
+++ src/popup.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export default class PopUp{
+    constructor(){
+    this.popUp = document.querySelector('.pop-up');
+    this.popUpMessage = document.querySelector('.pop-up__message');
+    this.popUpRefreshBtn = document.querySelector('.pop-up__refresh');
+    this.popUpRefresh.addEventListener('click',()=>{
+
+    }) 
+    }
+
+    setClickListener(onClick){
+        
+    }
+}
