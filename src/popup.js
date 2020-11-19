d781c685741f62dd31225652c46da2e3957e88a2
hesshess <64644262+hesshess@users.noreply.github.com>
2020-11-19 20:37:15 +0000

    PopUp Refactoring

--- src/popup.js
+++ src/popup.js
@@ -2,15 +2,26 @@
 
 export default class PopUp{
     constructor(){
-    this.popUp = document.querySelector('.pop-up');
-    this.popUpMessage = document.querySelector('.pop-up__message');
-    this.popUpRefreshBtn = document.querySelector('.pop-up__refresh');
-    this.popUpRefresh.addEventListener('click',()=>{
 
-    }) 
+        this.popUp = document.querySelector('.pop-up');
+        this.popUpText = document.querySelector('.pop-up__message');
+        this.popUpRefresh = document.querySelector('.pop-up__refresh');
+        this.popUpRefresh.addEventListener('click',()=>{
+            this.onClick && this.onClick();
+    })      hide();
     }
 
     setClickListener(onClick){
-        
+        this.onClick = onClick;
     }
+
+    hide(){
+        this.popUp.classList.add('pop-up--hide');
+    }
+
+    showWithText(text) {
+        this.popUpText.innerText = text;
+        this.popUp.classList.remove('pop-up--hide');
+      }
+      
 }
